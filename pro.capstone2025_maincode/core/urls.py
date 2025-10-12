c91b66e5d638ffa4eae4fea0075d6366ba8cfca8
Leiva-Gajardo-Herrera <pro.capstone1808@gmail.com>
2025-10-12 11:41:24 -0300

    Add files via upload

---
+++ pro.capstone2025_maincode/core/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import index, login_view, registro_view,ayuda_view, nosotros_view, propiedad_view, salir, mainregistrado_view,  gestordocumentos_view, propiedades_view
+
+app_name = 'core'
+urlpatterns = [ 
+    path('', index, name='index'),
+    path("login/", login_view, name="login"),
+    path("registro/", registro_view, name="registro"),
+    path('salir/', salir, name='salir'),
+    path("ayuda/", ayuda_view, name="ayuda"), 
+    path("nosotros/", nosotros_view, name="nosotros"), 
+    path("main-registrado/", mainregistrado_view, name="main-registrado"),
+    path("propiedades/", propiedades_view, name="propiedades"),
+    path("gestor-documentos/", gestordocumentos_view, name="gestor-documentos"),
+    path("propiedadcrud/", propiedad_view, name="propiedad"), 
+
+    
+
+
+]
