c44f476c4117645c38493c96645cfaa0c36a7504
Sebakite272 <seba.gajardo@duocuc.cl>
2025-11-20 01:06:18 -0300

    ok

--- pro.capstone2025_maincode/core/urls.py
+++ pro.capstone2025_maincode/core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+<<<<<<< Updated upstream
 from .views import index, login_view, registro_view, usereg_view, editado_view, ayuda_view, nosotros_view, propiedad_view, terrenoslistos_view, perfil_view, salir, mainregistrado_view, gestordocumentos_view, logout_views, propiedades_view, createform_view, editarform_view, misprop_view, propiedad_delete_view, ver_propiedades_view
+=======
+from .views import index, login_view, registro_view, usereg_view, editado_view, ayuda_view, nosotros_view, propiedadeliminar_view, propiedad_view, terrenoslistos_view, salir, mainregistrado_view, gestordocumentos_view, propiedades_view, propiedadform_usreg_view, createform_view, editarform_view, misprop_view, perfil_view
+>>>>>>> Stashed changes
 
 app_name = 'core'
 urlpatterns = [ 
@@ -23,9 +27,17 @@ urlpatterns = [
     path("usregistrado/", usereg_view, name="usregistrado"),
     path("editado/", editado_view, name="editado"),
     path("terrenoslistos/", terrenoslistos_view, name="terrenoslistos"),
+<<<<<<< Updated upstream
     path("perfil/", perfil_view, name="perfil"),
     path("logout/", logout_views, name="logout"),
     #path('propiedad/<int:pk>/eliminar/', eliminar_view, name='propiedad_eliminar'), 
+=======
+    path("propiedadform-usreg/", propiedadform_usreg_view, name="propiedadform_usreg"),
+    path("propiedades/", propiedades_view, name="propiedades"),
+    path( 'propiedad_eliminar/<int:pk>/eliminar/',
+        propiedadeliminar_view,
+        name='propiedad_eliminar' )
+>>>>>>> Stashed changes
     
     
 
